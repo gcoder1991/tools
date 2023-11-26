8e5a829151e330309314693f5a57f9bcf08f45fc
Relvf <gcoder1991@gmail.com>
2023-11-26 22:13:33 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,100 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use bytes::{Buf, Bytes};
+use http_body_util::{BodyExt, Full};
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{body::Incoming as IncomingBody, header, Method, Request, Response, StatusCode};
+use hyper_util::rt::TokioIo;
+use serde_json::Value;
+use tokio::net::{TcpListener, TcpStream};
+
+
+type GenericError = Box<dyn std::error::Error + Send + Sync>;
+type Result<T> = std::result::Result<T, GenericError>;
+type BoxBody = http_body_util::combinators::BoxBody<Bytes, hyper::Error>;
+
+static INDEX: &[u8] = b"<a href=\"test.html\">test.html</a>";
+static INTERNAL_SERVER_ERROR: &[u8] = b"Internal Server Error";
+static NOTFOUND: &[u8] = b"Not Found";
+static POST_DATA: &str = r#"{"original": "data"}"#;
+static URL: &str = "http://127.0.0.1:1337/json_api";
+
+async fn api_post_response(req: Request<IncomingBody>) -> Result<Response<BoxBody>> {
+    // Aggregate the body...
+    let whole_body = req.collect().await?.aggregate();
+    // Decode as JSON...
+    let mut data: Value = serde_json::from_reader(whole_body.reader())?;
+    // Change the JSON...
+    data["test"] = Value::from("test_value");
+    // And respond with the new JSON.
+    let json = serde_json::to_string(&data)?;
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .header(header::CONTENT_TYPE, "application/json")
+        .body(full(json))?;
+    Ok(response)
+}
+
+async fn api_get_response(req: Request<IncomingBody>) -> Result<Response<BoxBody>> {
+    let query = if let Some(q) = req.uri().query() {
+        q
+    } else {
+        return Ok(Response::builder()
+            .status(StatusCode::UNPROCESSABLE_ENTITY)
+            .body(full("no url!"))
+            .unwrap());
+    };
+
+    // 1 解析参数订阅地址url
+
+    // 2 获取请求返回信息并解析
+
+    // 3 整理并返回
+
+    let data = r#"
+    let me do
+    "#;
+    Ok(Response::builder()
+        .header(header::CONTENT_TYPE, "application/json")
+        .body(full(data)).unwrap())
+}
+
+async fn route(req: Request<IncomingBody>) -> Result<Response<BoxBody>> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/surge") => api_get_response(req).await,
+        _ => {
+            // Return 404 not found response.
+            Ok(Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(full(NOTFOUND))
+                .unwrap())
+        }
+    }
+}
+
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let addr: SocketAddr = "0.0.0.0:3100".parse().unwrap();
+
+    let listener = TcpListener::bind(&addr).await?;
+    println!("Listening on http://{}", addr);
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+
+        tokio::task::spawn(async move {
+            let service = service_fn(move |req| route(req));
+
+            if let Err(err) = http1::Builder::new().serve_connection(io, service).await {
+                println!("Failed to serve connection: {:?}", err);
+            }
+        });
+    }
 }
